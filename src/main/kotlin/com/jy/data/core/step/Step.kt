5bfc870ef982c35117f45585bafee45162f8b337
Je.Wang <Je.Wang@fanruan.com>
2021-02-01 19:19:51 +0800

    init

--- src/main/kotlin/com/jy/data/core/step/Step.kt
+++ src/main/kotlin/com/jy/data/core/step/Step.kt
@@ -1,37 +1,56 @@
 package com.jy.data.core.step
 
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.kotlin.Flowables
 import io.reactivex.rxjava3.kotlin.toFlowable
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
+import java.util.concurrent.TimeUnit
 
 interface Step {
     fun processRow() {
-        listOf(1, 2, 3)
-                .toFlowable()
-                .map { it.toString() }
-                .doOnEach(subscriber)
-                .subscribe {
-                    println(it)
-                }
     }
 }
 
-val subscriber = object : Subscriber<String> {
+data class Item(val name: String, val age: Int)
 
-    override fun onNext(item: String?) {
+fun main() {
+    Flowables.create<Item>(BackpressureStrategy.BUFFER) { emitter ->
+        listOf(Item("a", 2), Item("a", 2), Item("b", 1))
+            .forEach { item ->
+                emitter.onNext(item);
+            }
+    }.groupBy {
+            it.name
+        }
+        .delay(1000,TimeUnit.DAYS)
+        .subscribe { flow ->
+            flow
+                .groupBy { it.age }
+                .subscribe { newFlow ->
+                    newFlow
+                        .filter { it.age == 2 }
+                        .doOnEach(subscriber)
+                        .subscribe()
+                }
+        }
+}
+
+val subscriber = object : Subscriber<Item> {
+
+
+    override fun onNext(item: Item?) {
         println(item)
+        println(Thread.currentThread())
     }
 
     override fun onError(throwable: Throwable?) {
-        TODO("Not yet implemented")
     }
 
     override fun onComplete() {
-        TODO("Not yet implemented")
     }
 
     override fun onSubscribe(s: Subscription?) {
-        TODO("Not yet implemented")
     }
 
 }
